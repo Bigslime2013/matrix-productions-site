f2add5071d200541de298af5785142ce934bf3e7
Bigslime2013 <100032915+Bigslime2013@users.noreply.github.com>
2025-10-04 02:26:11 +0000

    Deploy to Cloudflare Pages

--- matrix-productions-site/modules/phantommodal/logic.js
+++ matrix-productions-site/modules/phantommodal/logic.js
@@ -1,8 +1,12 @@
 // phantomModal.js
+import { startMatrixRain, stopMatrixRain } from './matrixRain.js';
+
 export function openPhantomModal() {
   document.getElementById('phantomModal').style.display = 'block';
+  startMatrixRain();
 }
 
 export function closePhantomModal() {
   document.getElementById('phantomModal').style.display = 'none';
+  stopMatrixRain();
 }
